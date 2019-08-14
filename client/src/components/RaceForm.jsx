41921b47ad9b0bc0dd48848c6658b92c97436cfb
XX <xx@XXs-MBP13.local>
2019-08-14 19:05:55 -0400

    fixing race form labels

--- client/src/components/RaceForm.jsx
+++ client/src/components/RaceForm.jsx
@@ -6,61 +6,71 @@ function RaceForm(props) {
     <div className="race-form">
       <h2>Create a new race</h2>
       <form onSubmit={props.newRace}>
-        <p>Race name:</p>
+        <label for="name">Race name</label>
         <input
           type="text"
           name="name"
           value={props.raceForm.name}
-          onChange={props.handleRaceFormChange} />
-        <p>Race date:</p>
+          onChange={props.handleRaceFormChange}
+        />
+        <label for="date">Date</label>
         <input
           type="text"
           name="date"
           placeholder="yyyy-mm-dd"
           value={props.raceForm.date}
-          onChange={props.handleRaceFormChange} />
-        <p>Decription:</p>
+          onChange={props.handleRaceFormChange}
+        />
+        <label for="description">Description</label>
         <input
           type="text"
           name="description"
           value={props.raceForm.description}
-          onChange={props.handleRaceFormChange} />
-        <p>City:</p>
+          onChange={props.handleRaceFormChange}
+        />
+        <label for="city">City</label>
         <input
           type="text"
           name="city"
           value={props.raceForm.city}
-          onChange={props.handleRaceFormChange} />
-        <p>State:</p>
+          onChange={props.handleRaceFormChange}
+        />
+        <label for="state">State</label>
         <input
           type="text"
           name="state"
           value={props.raceForm.state}
-          onChange={props.handleRaceFormChange} />
-        <p>Country:</p>
+          onChange={props.handleRaceFormChange}
+        />
+        <label for="country">Country</label>
         <input
           type="text"
           name="country"
           value={props.raceForm.country}
-          onChange={props.handleRaceFormChange} />
-        <p>Organization:</p>
+          onChange={props.handleRaceFormChange}
+        />
+        <label for="organization">Organizer</label>
         <input
           type="text"
           name="organization"
           value={props.raceForm.organization}
-          onChange={props.handleRaceFormChange} />
-        <p>Distance:</p>
+          onChange={props.handleRaceFormChange}
+        />
+        <label for="distance">Distance (mile)</label>
         <input
           type="text"
           name="distance"
           value={props.raceForm.distance}
-          onChange={props.handleRaceFormChange} />
-        <p>Website:</p>
+          onChange={props.handleRaceFormChange}
+        />
+        <label for="website">Website</label>
         <input
           type="text"
           name="website"
           value={props.raceForm.website}
-          onChange={props.handleRaceFormChange} />
+          onChange={props.handleRaceFormChange}
+          placeholder="http://..."
+        />
         <input
           type="hidden"
           name="user_id"
